471df9f014b1d64380ed52016ee91c59b899de10
Utkarshende <139004110+Utkarshende@users.noreply.github.com>
2025-12-27 20:54:59 +0530

    update:github and live link url in project.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,28 +3,28 @@ export const projects = [
     title: "Pro-Tasker",
     description: "Minimalist Full-Stack Project Management App with a Kanban board and real-time updates.",
     tech: ["React", "Node.js", "MongoDB", "Tailwind"],
-    link: "https://pro-tasker-demo.vercel.app", // Replace with your Vercel URL
+    link: "https://pro-tasker-chi.vercel.app/", 
     github: "https://github.com/Utkarshende/pro-tasker"
   },
   {
     title: "MERN Blog",
     description: "High-performance blogging platform with JWT auth and Cloudinary image hosting.",
     tech: ["React", "Express", "Cloudinary", "JWT"],
-    link: "https://mern-blog-demo.vercel.app", // Replace with your Vercel URL
+    link: "https://mern-blog-client-ten-vert.vercel.app/" ,
     github: "https://github.com/Utkarshende/mern-blog"
   },
   {
     title: "MERN Chat",
     description: "Real-time chat platform using WebSockets for instant data flow.",
     tech: ["Socket.io", "Node.js", "React", "MongoDB"],
-    link: "https://mern-chat-demo.vercel.app", // Replace with your Vercel URL
+    link: "https://mern-chat-api-azure.vercel.app/", 
     github: "https://github.com/Utkarshende/mern-chat-api"
   },
   {
     title: "MERN Todo List",
     description: "Secure task management featuring JWT auth and a clean dashboard UI.",
     tech: ["React", "Node.js", "Render", "Vercel"],
-    link: "https://mern-todo-list-frontend.vercel.app", // Replace with your Vercel URL
-    github: "https://github.com/Utkarshende/mern-todo-list"
+    link:"https://github.com/Utkarshende/mern-todo-list", 
+    github: "https://2domern.netlify.app/"
   }
 ];
